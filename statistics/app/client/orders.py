b6d8cc3b3fa1ec43b7dfe6ee1b1587de5520a3cc
wattox00 <korschzozinho@gmail.com>
2026-01-19 13:43:29 +0100

    Price calculation order by date missing order by quant missing

--- statistics/app/client/orders.py
+++ statistics/app/client/orders.py
@@ -2,5 +2,7 @@ import httpx
 
 MENU_SERVICE_URL = "http://127.0.0.1:8002"
 
-def get_menu_items():
-    pass
+def get_order_items():
+    response = httpx.get(f"{MENU_SERVICE_URL}/view_orders")
+    response.raise_for_status()
+    return response.json()
--- statistics/app/services/statistics_services.py
+++ statistics/app/services/statistics_services.py
@@ -1,6 +1,33 @@
-
-
+from client.orders import get_order_items
+from client.master_data import get_menu_items
 
 class MostSoldItems():
     def most_sold_items(self):
-        pass
+        menu = get_menu_items() # array -> objets (key=id) -> food_name: str ; price: int
+        orders = get_order_items() # array -> objects (key=id) -> food_names: array(str)
+
+        most_sold_items_list = {} # item_name: quantity
+
+        for order in orders:
+            for food_name in order["food_names"]:
+                if food_name in most_sold_items_list:
+                    most_sold_items_list[food_name] += 1
+                else:
+                    most_sold_items_list[food_name] = 1
+        
+        menu_prices = {
+            m["food_name"]: m["price"]
+            for m in menu
+        }
+
+        revenue_by_item = {}
+
+        for item_name, quantity in most_sold_items_list.items():
+            price = menu_prices.get(item_name)
+
+            if price is None:
+                continue
+
+            revenue_by_item[item_name] = quantity * price
+
+        return revenue_by_item
