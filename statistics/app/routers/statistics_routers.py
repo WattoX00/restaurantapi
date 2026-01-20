cdc508f3ba8f530f5c08a6cdfdef63effb475438
wattox00 <korschzozinho@gmail.com>
2026-01-20 11:34:02 +0100

    routes for statistics

--- statistics/app/routers/statistics_routers.py
+++ statistics/app/routers/statistics_routers.py
@@ -6,3 +6,11 @@ router = APIRouter()
 @router.get("/most_items_sold")
 def get_most_items_sold(start_date: date, end_date: date):
     pass
+
+@router.get("/least_items_sold")
+def get_least_items_sold():
+    pass
+
+@router.get("/monthly_data")
+def get_monthly_data():
+    pass
