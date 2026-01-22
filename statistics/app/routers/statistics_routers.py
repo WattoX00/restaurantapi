2b73190e919e268fabe46755bac9c3e7e200eba3
Wattox <78638839+WattoX00@users.noreply.github.com>
2026-01-22 13:26:35 +0100

    Update statistics_routers.py

--- statistics/app/routers/statistics_routers.py
+++ statistics/app/routers/statistics_routers.py
@@ -1,17 +1,18 @@
 from fastapi import APIRouter
 from datetime import date
-from services.statistics_services import MostSoldItems
+from services.statistics_services import MostSoldItems, LeastItemsSold
 router = APIRouter()
 
 most_sold_items_list = MostSoldItems()
+least_sold_items_list = LeastItemsSold()
 
 @router.get("/most_items_sold")
 def get_most_items_sold(start_date: date, end_date: date = date.today()):
     return most_sold_items_list.most_sold_items(start_date, end_date)
 
 @router.get("/least_items_sold")
-def get_least_items_sold():
-    pass
+def get_least_items_sold(start_date: date, end_date: date = date.today()):
+    return least_sold_items_list.least_items_sold(start_date, end_date)
 
 @router.get("/monthly_data")
 def get_monthly_data():
