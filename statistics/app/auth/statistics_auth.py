a0cae704fa5c110486a8eb46ae070d8248c9864b
wattox00 <korschzozinho@gmail.com>
2026-03-09 12:41:57 +0100

    Statistics auth added

---
+++ statistics/app/auth/statistics_auth.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+from fastapi import HTTPException, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+from .statistics_config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+
+security = HTTPBearer()
+
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "admin"
+
+
+def authenticate_user(username: str, password: str):
+    if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+        return {"username": username}
+    return None
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def verify_token(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+):
+    token = credentials.credentials
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
