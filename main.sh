90421b5ea582d546874c62b5076f3c4f4874bc65
wattox00 <korschzozinho@gmail.com>
2026-03-09 14:08:55 +0100

    Main.sh prints out the frontend html file

--- main.sh
+++ main.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+FRONTEND="$SCRIPT_DIR/frontend/index.html"
+
 python master-data/app/main.py &
 python orders/app/main.py &
 python statistics/app/main.py &
+
+echo ""
+echo "Servers started."
+echo "Open the frontend:"
+echo "file://$FRONTEND"
+echo ""
 
 wait
