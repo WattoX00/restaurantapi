2f02994fd3687b2d19ba59f8a66f806e4aff4f2d
wattox00 <korschzozinho@gmail.com>
2026-03-15 14:45:06 +0100

    Client bug fix

--- statistics/app/client/orders.py
+++ statistics/app/client/orders.py
@@ -3,6 +3,6 @@ import httpx
 MENU_SERVICE_URL = "http://127.0.0.1:8002"
 
 def get_order_items():
-    response = httpx.get(f"{MENU_SERVICE_URL}/view_orders")
+    response = httpx.get(f"{MENU_SERVICE_URL}/view_finished")
     response.raise_for_status()
     return response.json()
